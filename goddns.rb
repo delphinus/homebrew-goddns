6caaa4591d6f055576cc07b03069557e672c2d92
delphinus <me@delphinus.dev>
2019-03-25 19:09:00 +0900

    Update to v1.1.1

--- goddns.rb
+++ goddns.rb
@@ -1,8 +1,8 @@
 class Goddns < Formula
   desc "Dynamic DNS Updater for Google Domains"
   homepage "https://github.com/delphinus/goddns"
-  url "https://github.com/delphinus/goddns/archive/v1.1.0.tar.gz"
-  sha256 "887a7ae94e87e4a1335c514c8f62fde30d5744cecf26fdd6782c891a7ad42ae1"
+  url "https://github.com/delphinus/goddns/archive/v1.1.1.tar.gz"
+  sha256 "27941ef07c0a3c4ad01f3959f4a2b8b93456aac36ec28496f473f0c4c04bef44"
   head "https://github.com/delphinus/goddns.git"
 
   depends_on "go" => :build
